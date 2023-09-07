aa975fc4c90b51632b68a6ab8a212c4c59ba42ce
Santiago Barrera Escudero <prv_santiago.barrera@dale.com.co>
2023-09-06 21:03:06 -0500

    style: :art: format code at http request

--- src/services/http-request.ts
+++ src/services/http-request.ts
@@ -1,21 +1,24 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from 'axios';
 
 export const get = async (endpoint: string): Promise<AxiosResponse> => {
   try {
-    const result = await axios.get(endpoint)
-    return result
+    const result = await axios.get(endpoint);
+    return result;
   } catch (error) {
-    console.error(error)
-    throw new Error('Error trying to get at axios request')
+    console.error(error);
+    throw new Error('Error trying to get at axios request');
   }
-}
+};
 
-export const post = async (endpoint: string, data: unknown): Promise<AxiosResponse> => {
+export const post = async (
+  endpoint: string,
+  data: unknown
+): Promise<AxiosResponse> => {
   try {
-    const result = await axios.post(endpoint, data)
-    return result
+    const result = await axios.post(endpoint, data);
+    return result;
   } catch (error) {
-    console.error(error)
-    throw new Error('Error trying to post at axios request')
+    console.error(error);
+    throw new Error('Error trying to post at axios request');
   }
-}
+};
